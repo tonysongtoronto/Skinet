43b503cc4c69bed6fadb3adcf42c567f3c6dac76
tony <songjf@yahoo.com>
2025-07-29 00:12:47 -0400

    init Create Project

---
+++ skinet/API/Program.cs
@@ -0,0 +1,25 @@
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+
+var app = builder.Build();
+
+
+
+
+app.MapControllers();
+
+app.Run();
