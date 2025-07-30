484936e2556cc3595ed625ff957d768b9813997b
tony <songjf@yahoo.com>
2025-07-29 22:31:26 -0400

    section 3

---
+++ skinet/Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Reflection;
+using System.Text.Json;
+using Core.Entities;
+
+namespace Infrastructure.Data;
+
+public class StoreContextSeed
+{
+    public static async Task SeedAsync(StoreContext context)
+    {
+        if (!context.Products.Any())
+        {
+
+    //     var basePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+    //   var filePath = Path.Combine(basePath!, "Infrastructure", "Data", "SeedData", "products.json");
+
+
+           // var productsData = await File.ReadAllTextAsync(filePath);
+
+            var productsData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/products.json");
+            var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+
+            if (products == null) return;
+
+            context.Products.AddRange(products);
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
