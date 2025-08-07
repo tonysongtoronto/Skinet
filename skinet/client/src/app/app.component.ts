ffda24d510a9e9ad6d5367d34f26c673c6ce8c82
tony <songjf@yahoo.com>
2025-08-07 02:31:17 -0400

    section 9

--- skinet/client/src/app/app.component.ts
+++ skinet/client/src/app/app.component.ts
@@ -2,27 +2,22 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "../layout/header/header.component";
 import { HttpClient } from '@angular/common/http';
-import { Product } from '../models/product';
-import { Pagination } from '../models/pagination';
+import { Product } from './shared/models/product';
+import { ShopService } from './core/services/shop.service';
+import { ShopComponent } from "./features/shop/shop.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
 
-  baseUrl = 'https://localhost:5001/api/';
-  private http = inject(HttpClient);
+
   title = 'skinet';
-  products: Product[] = [];
 
-  ngOnInit() {
-    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
-      next: response => this.products = response.data,
-      error: error => console.error(error),
-      complete: () => console.log('complete')
-    })
-  }
+
+
 }
