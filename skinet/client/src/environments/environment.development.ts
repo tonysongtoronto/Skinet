0fe2b4d695eafc06fb9eda5271fe04a7c06e4f2d
tony <songjf@yahoo.com>
2025-08-22 03:51:39 -0400

    section 1604

--- skinet/client/src/environments/environment.development.ts
+++ skinet/client/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   baseUrl: 'https://localhost:5001/api/',
-   stripePublicKey:''
+
 
 };
--- skinet/client/src/environments/environment.ts
+++ skinet/client/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   baseUrl: 'api/',
-  stripePublicKey:''
+
 
 };
