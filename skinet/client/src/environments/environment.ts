6d8df019435b27f341fd170ed1f32d282c03a176
tony <songjf@yahoo.com>
2025-08-22 03:49:04 -0400

    section 1602

--- skinet/client/src/environments/environment.ts
+++ skinet/client/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   baseUrl: 'api/',
   stripePublicKey:''
-   // stripePublicKey: 'pk_test_51RwEmZ6G9gbT8y8T9dBNI8rGTgFM0EX4wUIddv07Yo17DwTzBk2Vguiwmz8SinIUffPe27YrIJ85C8jNEBRXOrUm00rKpNYi2u'
+
 };
